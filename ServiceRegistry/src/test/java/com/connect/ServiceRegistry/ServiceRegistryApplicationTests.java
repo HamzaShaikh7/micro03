1b1707cecae3fabb8920c3fc331a211c0c543f8e
HamzaShaikh7 <imhamzase7en@gmail.com>
2025-05-06 15:29:01 +0530

    Added Service Registry project.

---
+++ ServiceRegistry/src/test/java/com/connect/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connect.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
